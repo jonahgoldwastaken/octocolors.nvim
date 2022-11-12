358530e583e0f35a41f2b5d4c8819fe89223ef4f
Jonah Meijers <jonahmeijers97@gmail.com>
2022-11-12 13:39:15 +0100

    change lualine background

--- lua/lualine/themes/octocolors.lua
+++ lua/lualine/themes/octocolors.lua
@@ -1,22 +1,21 @@
 local colors = require("octocolors.colors").setup()
 local util = require("octocolors.util")
 
-local bg1 = colors.canvas.default
-local bg2 = colors.canvas.inset
+local bg = colors.canvas.default
 local fg = colors.fg.default
 
 local octocolors = {}
 
 local function create_group(scale)
 	local group = {
-		a = { bg = util.light_dark(scale[6], scale[4]), fg = bg1, gui = "bold" },
+		a = { bg = util.light_dark(scale[6], scale[4]), fg = bg, gui = "bold" },
 		b = { bg = util.light_dark(scale[7], scale[8]), fg = scale[1] },
 		c = { bg = colors.canvas.overlay, fg = fg },
 	}
 	return group
 end
 
-local inactive_hi = { bg = bg2, fg = util.alpha(fg, bg2, 0.3) }
+local inactive_hi = { bg = colors.canvas.subtle, fg = util.alpha(fg, bg, 0.3) }
 
 octocolors.normal = create_group(colors.scale.blue)
 octocolors.insert = create_group(colors.scale.green)
