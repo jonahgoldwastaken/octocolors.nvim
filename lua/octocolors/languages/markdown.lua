2fe06ca8e1e7228fd6666b678f60c82381b35823
Jonah Meijers <jonahmeijers97@gmail.com>
2023-01-11 15:59:42 +0100

    change colors for markdown and rust

--- lua/octocolors/languages/markdown.lua
+++ lua/octocolors/languages/markdown.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.highlights(c)
 	return {
 		["@text.title.markdown"] = { fg = c.blue2, bold = true },
-		["@text.reference.markdown_inline"] = { fg = c.blue2 },
+		["@punctuation.special.markdown"] = { fg = c.blue2 },
 	}
 end
 return M
--- lua/octocolors/languages/rust.lua
+++ lua/octocolors/languages/rust.lua
@@ -4,7 +4,13 @@ local M = {}
 function M.highlights(c)
 	return {
 		["@punctuation.delimiter.rust"] = { fg = c.red },
-		["@type.rust"] = { fg = c.orange },
+		--[[ ["@type.rust"] = { fg = c.orange }, ]]
+		["@function.macro.rust"] = { fg = c.purple },
+		["@type.builtin.rust"] = { fg = c.blue2 },
+		["@variable.builtin.rust"] = { fg = c.red },
+		["@interface.defaultLibrary.rust"] = { fg = c.blue2 },
+		["@typeAlias.library.rust"] = { fg = c.orange },
+		["@interface.library.rust"] = { fg = c.orange },
 	}
 end
 
