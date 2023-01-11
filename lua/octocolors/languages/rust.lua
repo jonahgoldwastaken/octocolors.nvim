d370629d66099ba1b730095d871fac345f209ad8
Jonah Meijers <jonahmeijers97@gmail.com>
2023-01-11 22:06:48 +0100

    improvements in rust coloring

--- lua/octocolors/languages/rust.lua
+++ lua/octocolors/languages/rust.lua
@@ -3,9 +3,10 @@ local M = {}
 
 function M.highlights(c)
 	return {
-		["@punctuation.delimiter.rust"] = { fg = c.red },
+		-- ["@punctuation.delimiter.rust"] = { fg = c.fg },
 		--[[ ["@type.rust"] = { fg = c.orange }, ]]
-		["@function.macro.rust"] = { fg = c.purple },
+		["@function.macro.rust"] = { fg = c.fg },
+		["@formatSpecifier"] = { fg = c.red },
 		["@type.builtin.rust"] = { fg = c.blue2 },
 		["@variable.builtin.rust"] = { fg = c.red },
 		["@interface.defaultLibrary.rust"] = { fg = c.blue2 },
